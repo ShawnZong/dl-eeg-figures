668f44427eef4f54d82bf3fd01af23e8c038a574
tanj <junsheng.tan@eurecom.fr>
2021-11-29 00:53:49 +0100

    forwardRef successfully passed to responsivebar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { render } from "react-dom";
 
 import { MyResponsiveBar } from "./components/Bar";
 import { MyResponsivePie } from "./components/PieChart";
 import { MyResponsiveSwarmPlot } from "./components/SwarmPlot";
 import { MyResponsiveChoropleth } from "./components/Chroropleth";
+import {
+  exportComponentAsJPEG,
+  exportComponentAsPDF,
+  exportComponentAsPNG,
+} from "react-component-export-image";
 
 const styles = {
   fontFamily: "sans-serif",
@@ -12,14 +17,23 @@ const styles = {
   width: 1000,
   height: 500,
 };
-
+const ComponentToPrint = React.forwardRef((props, ref) => (
+  <React.Fragment ref={ref}>
+    <MyResponsiveBar />
+  </React.Fragment>
+));
 const App = () => (
   <div style={styles}>
-    <MyResponsiveBar />
-    <MyResponsivePie />
-    <MyResponsiveSwarmPlot />
-    <MyResponsiveChoropleth />
+    <ComponentToPrint />
   </div>
 );
+// const App = () => (
+//   <div style={styles}>
+//     <MyResponsiveBar />
+//     <MyResponsivePie />
+//     <MyResponsiveSwarmPlot />
+//     <MyResponsiveChoropleth />
+//   </div>
+// );
 
 render(<App />, document.getElementById("root"));
